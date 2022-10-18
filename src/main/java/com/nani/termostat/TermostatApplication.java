a6d8c7862fd30bf8c73bece3660ccdefbb4f7aea
nandoUrbanIt <48318901+Nundi16@users.noreply.github.com>
2022-10-18 20:52:27 +0200

    Initial commit

---
+++ src/main/java/com/nani/termostat/TermostatApplication.java
@@ -0,0 +1,13 @@
+package com.nani.termostat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermostatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermostatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nani/termostat/TermostatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nani.termostat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermostatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
